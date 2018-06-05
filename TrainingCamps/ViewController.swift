4180eaf16661192aeaafed2dedbf4ce1ea793eef
Steven Lord <stevenlord@me.com>
2018-06-05 15:07:13 +0100

    Initial Commit

---
+++ TrainingCamps/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrainingCamps
+//
+//  Created by Steven Lord on 05/06/2018.
+//  Copyright © 2018 Steven Lord. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
