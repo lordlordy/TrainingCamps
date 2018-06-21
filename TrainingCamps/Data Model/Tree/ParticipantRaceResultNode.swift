244cd913a61ea8fb28fac896a08adce3947ec203
Steven Lord <stevenlord@me.com>
2018-06-21 13:27:57 +0100

    missed

---
+++ TrainingCamps/Data Model/Tree/ParticipantRaceResultNode.swift
@@ -0,0 +1,19 @@
+//
+//  ParticipantRaceResultNode.swift
+//  TrainingCamps
+//
+//  Created by Steven Lord on 14/06/2018.
+//  Copyright © 2018 Steven Lord. All rights reserved.
+//
+
+import Foundation
+
+class ParticipantRaceResultNode: RaceResultNode{
+    
+    
+    override init(raceResult: RaceResult){
+        super.init(raceResult: raceResult)
+        name = raceResult.race!.name
+    }
+
+}
